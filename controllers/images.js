f72e8d7b335dc80e54a21230d4577e82c7ce044c
dkondrashin <dkflasher@yandex.ru>
2017-03-12 15:16:41 +0500

    controllers

---
+++ controllers/images.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Quest = require('../models/Quest');
+
+exports.create = (req, res) => {
+/*    const image = new Quest({
+        imageId: req.body.imageId,
+        path: req.body.path,
+        answer: req.body.answer
+    });
+
+    image.save();
+
+    // Не позволяем отправлять форму дважды
+    res.redirect(302, '/images');*/
+};
+
+exports.update = (req, res) => {
+
+}
+
+exports.delete = (req, res) => {
+
+}
---
+++ controllers/like.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Quest = require('../models/Quest');
+
+exports.like = (req, res) => {
+
+};
+
+exports.unlike = (req, res) => {
+
+};
+
+
+
+
