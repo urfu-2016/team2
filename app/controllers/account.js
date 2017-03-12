fcf97e18fec4892ff32325e744584f1660b53bb4
dkondrashin <dkflasher@yandex.ru>
2017-03-12 16:10:05 +0500

    заготовка для контроллеров исправлено

---
+++ app/controllers/account.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/*const Quest = require('../models/Quest');*/
+
+
+exports.register = (req, res) => {
+/*    const user = new Quest({
+        name: req.body.name,
+        password: req.body.password,
+        userId: req.body.userId
+    });
+
+    user.save();
+
+    // Не позволяем отправлять форму дважды
+    res.redirect(302, '/registration');*/
+};
+
+
+// получить форму регистрации
+exports.registration = (req, res) => {
+
+};
+
+exports.management = (req, res) => {
+
+};
+
+// обновить инфу о пользователе
+exports.user = (req, res) => {
+
+};
