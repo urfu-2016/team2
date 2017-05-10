06628b7e356353018bfeb4403079020f5fb9c028
Semen Makhaev <semenmakhaev@yandex.ru>
2017-05-10 11:20:30 +0500

    Исправил баг с приветствием пользователя в шапке

--- app/middlewares/username.js
+++ app/middlewares/username.js
@@ -1,3 +1,12 @@
+'use strict';
+
 /**
- * Created by Semen Makhaev on 10.05.2017.
+ * Middleware для записи имени пользователя в локальные переменные
+ * @param req
+ * @param res
+ * @param next
  */
+module.exports = (req, res, next) => {
+    res.locals['username'] = req.isAuthenticated() ? req.user.username : '';
+    next();
+};
